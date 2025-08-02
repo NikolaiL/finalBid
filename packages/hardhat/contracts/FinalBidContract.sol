51463a2d6f3667b5c0fa580078d0b16f80fc9ba9
Nikolai <nikolai1973@gmail.com>
2025-08-02 12:33:37 +0200

    added ownable functions from openzeppelin

--- packages/hardhat/contracts/FinalBidContract.sol
+++ packages/hardhat/contracts/FinalBidContract.sol
@@ -5,16 +5,15 @@ pragma solidity >=0.8.0 <0.9.0;
 import "hardhat/console.sol";
 
 // Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * A smart contract that allows changing a state variable of the contract and tracking the changes
  * It also allows the owner to withdraw the Ether in the contract
  * @author BuidlGuidl
  */
-contract FinalBidContract {
+contract FinalBidContract is Ownable {
     // State Variables
-    address public immutable owner;
     string public greeting = "Building Unstoppable Apps!!!";
     bool public premium = false;
     uint256 public totalCounter = 0;
@@ -25,16 +24,8 @@ contract FinalBidContract {
 
     // Constructor: Called once on contract deployment
     // Check packages/hardhat/deploy/00_deploy_your_contract.ts
-    constructor(address _owner) {
-        owner = _owner;
-    }
-
-    // Modifier: used to define a set of rules that must be met before or after a function is executed
-    // Check the withdraw() function
-    modifier isOwner() {
-        // msg.sender: predefined variable that represents address of the account that called the current function
-        require(msg.sender == owner, "Not the Owner");
-        _;
+    constructor(address _owner) Ownable(_owner) {
+        // Owner is set in the Ownable constructor
     }
 
     /**
@@ -64,10 +55,10 @@ contract FinalBidContract {
 
     /**
      * Function that allows the owner to withdraw all the Ether in the contract
-     * The function can only be called by the owner of the contract as defined by the isOwner modifier
+     * The function can only be called by the owner of the contract as defined by the onlyOwner modifier
      */
-    function withdraw() public isOwner {
-        (bool success, ) = owner.call{ value: address(this).balance }("");
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner().call{ value: address(this).balance }("");
         require(success, "Failed to send Ether");
     }
 
